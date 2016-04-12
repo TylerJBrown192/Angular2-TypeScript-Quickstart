d73241aeec4672db34d61eeafbac79ed97d26b0a
Tyler Brown <tylerjbrown192@gmail.com>
2016-04-12 14:54:32 -0700

    Add main.ts file in app directory with file structure explanations

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import {bootstrap}    from 'angular2/platform/browser'; //Specifies bootstrap function for browser applications. Different environments / platforms could take different components.
+import {AppComponent} from './app.component'; //import AppComponent class from root app.component.ts file
+
+bootstrap(AppComponent);
+
+// Creating main.ts file to demonstrate proper Angular application structure. Application bootstrapping is a separate concern from presenting a view (e.g. we may launch AppComponent in multiple environments with different bootstrappers.)
