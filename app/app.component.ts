b5c9862e68319d84907eb5832f308465165f8803
Tyler Brown <tylerjbrown192@gmail.com>
2016-04-12 15:44:29 -0700

    Index.html file added and explained, more import explanation in main.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core'; //Importing Angular's Component decorat
 
 @Component({ // Decorator function that accepts metadata object, which tells Angular how to create and use this component
     selector: 'my-app', //CSS selector for the HTML element that represents this component
-    template: '<h1>My First Angular 2 App</h1>' //Specifies the component's companion template. A more advanced template would contain data bindings to component properties and may identify other app components which have their own templates.
+    template: '<h1>Angular 2 & TypeScript</h1>' //Specifies the component's companion template. A more advanced template would contain data bindings to component properties and may identify other app components which have their own templates.
 })
 
 export class AppComponent { } //Root component, consistent in every Angular app. Further in app development, this class will expand with properties and further application logic.
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 import {bootstrap}    from 'angular2/platform/browser'; //Specifies bootstrap function for browser applications. Different environments / platforms could take different components.
-import {AppComponent} from './app.component'; //import AppComponent class from root app.component.ts file
+import {AppComponent} from './app.component'; //import AppComponent class from root app.component.ts file. Filename extension isn't specified because TS is transpiled to JS before running application
 
 bootstrap(AppComponent);
 
